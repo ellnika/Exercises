2320bf521d7fb6e6173cd0205909c180b9e08ca9
Ewa Czekalska <ewac@microsoft.com>
2016-09-04 19:01:52 +0100

    Initial Commit

---
+++ Exercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercises
+//
+//  Created by ewac on 04/09/2016.
+//  Copyright © 2016 Ewa Czekalska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exercises/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exercises
+//
+//  Created by ewac on 04/09/2016.
+//  Copyright © 2016 Ewa Czekalska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
