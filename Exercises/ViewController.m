30a2508e21763596df1c25f96ad52f1577eb7d33
Ewa Czekalska <ewac@microsoft.com>
2016-09-04 19:23:06 +0100

    Add existing file

--- Exercises/ViewController.m
+++ Exercises/ViewController.m
@@ -8,20 +8,40 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *array;
 
 @end
 
 @implementation ViewController
+- (IBAction)buttonTapped:(id)sender {
+    [self exerciseOne];
+    //    [self exerciseTwo];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self. view.backgroundColor=[UIColor greenColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)exerciseOne {
+    int N = 3;
+    self.array = @[@1, @3, @4];
+    
+    
+}
+
+- (void)setupInput {
+    
+    
 }
 
 @end
